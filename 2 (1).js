7788c8bec2e908157089979280bb77f596be5ff0
sadunikaushalya <sadunikaushalya2025@gmail.com>
2026-05-15 07:48:49 +0530

    Add files via upload

---
+++ 2 (1).js
@@ -0,0 +1,16 @@
+console.log(12=="12");//true
+console.log(12==="12");//false
+console.log(12!=12);//false
+console.log(12>10);//true
+console.log(12<8);//false
+console.log(12<=20);//true
+console.log(12>=4);//false
+
+let age=20;
+let hasid=true;
+
+console.log(age>=18&&hasid===true);//true
+console.log(age>=18||hasid===false);//true
+console.log(!hasid);//false
+
+
