ee43aef94f943971534f74448a90f853f5482f0f
sadunikaushalya <sadunikaushalya2025@gmail.com>
2026-06-05 23:00:06 +0530

    Add files via upload

---
+++ practical 1/4.js
@@ -0,0 +1,13 @@
+function create(){
+	let FName=document.getElementById("FName").value;
+	let LName=document.getElementById("LName").value;
+	
+	let Age=document.getElementById("Age").value;
+document.getElementById("result").value=FName+" "+LName+"-"+Age;}
+	
+function buy(product){
+alert(product+ " Selected");}
+
+document.getElementById("change").
+	addEventListener("click",function(){
+	document.body.style.backgroundColor="pink";})
