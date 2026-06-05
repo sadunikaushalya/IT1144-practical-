6aed8ebcc607856927eb44d0d5cffa907ddcde34
sadunikaushalya <sadunikaushalya2025@gmail.com>
2026-06-05 22:47:04 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,5 @@
+const title=document.getElementById("title");
+const button=document.getElementById("changeBtn");
+button.addEventListener("click",function(){
+	title.textContent="Button Clicked!";
+title.style.color="red";})
